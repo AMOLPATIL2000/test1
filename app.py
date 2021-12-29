5e33a3f06824337df9f4171ff19a6935c8c53563
dkteds <95552747+dkteds@users.noreply.github.com>
2021-12-29 09:14:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return "<h1>Hello</h1>"     
+   return render_template('first.html')    
    
 if __name__ == "__main__":
     print("Starting Python Flask Server For Home Price Prediction...")    
